5b0c4926275f28857a386f29700f8f0d3ed69ef6
Yuriy <yurius13@ukr.net>
2024-08-15 20:25:03 +0300

    Contacts with icons

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,27 @@
+import { FaUser } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa6";
+import css from "./Contact.module.css"
+
+const Contact = ({ contact: { id, name, number }, onDeleteContact }) => {
+    return (
+        <div className={css.contactCard}>
+            <div className={css.contactInfo}>
+                <p className={css.contactText}>
+                    <FaUser />
+                    {name}
+                </p>
+                <p className={css.contactText}>
+                    <FaPhone />
+                    {number}
+                </p>
+            </div>
+            <button
+                className={css.contactDeleteBtn}
+                type="button"
+                onClick={() => onDeleteContact(id)}
+            >
+                Delete
+            </button>
+        </div>
+    );
+};
